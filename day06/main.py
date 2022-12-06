e76e8ed1b0879154e264144aaeb1de03d7d067ff
Laura Galbraith <laura_b_galbraith@msn.com>
2022-12-06 14:10:48 -0800

    Day 6: clean up helper function and slightly improve performance

--- day06/main.py
+++ day06/main.py
@@ -13,17 +13,20 @@ from util.pyfileutil.fileutil import read_ascii_file_lines
 def find_x_unique_chars(stream, x):
   char_count = {}
   for i in range(0,len(stream)):
+    # add in newest character
+    # (dictionary value default is 0)
+    if stream[i] not in char_count:
+      char_count[stream[i]] = 0
+
+    char_count[stream[i]] += 1
+
     # remove (x+1)th oldest character from count
     if i >= x:
       dropped_char = stream[i-x]
       char_count[dropped_char] -= 1
       if char_count[dropped_char] == 0:
         char_count.pop(dropped_char)
-    # add in newest character
-    # (dictionary value default is 0)
-    if stream[i] not in char_count:
-      char_count[stream[i]] = 0
-    char_count[stream[i]] += 1
+
     # check how many unique characters have been seen
     if len(char_count) == x:
       return i+1
