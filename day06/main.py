f5eae6432bc523c3d65f39e782d4a91ca578e309
Laura Galbraith <laura_b_galbraith@msn.com>
2022-12-06 14:08:13 -0800

    Day 6 Puzzle 2 solved

--- day06/main.py
+++ day06/main.py
@@ -9,27 +9,34 @@ pylintrc sourced from https://google.github.io/styleguide/pyguide.html
 
 from util.pyfileutil.fileutil import read_ascii_file_lines
 
+# returns the number of characters that ends with x unique characters
+def find_x_unique_chars(stream, x):
+  char_count = {}
+  for i in range(0,len(stream)):
+    # remove (x+1)th oldest character from count
+    if i >= x:
+      dropped_char = stream[i-x]
+      char_count[dropped_char] -= 1
+      if char_count[dropped_char] == 0:
+        char_count.pop(dropped_char)
+    # add in newest character
+    # (dictionary value default is 0)
+    if stream[i] not in char_count:
+      char_count[stream[i]] = 0
+    char_count[stream[i]] += 1
+    # check how many unique characters have been seen
+    if len(char_count) == x:
+      return i+1
+
+  return -1
+
 # Parse input
-stream = read_ascii_file_lines('input.txt')[0]
+input_stream = read_ascii_file_lines('input.txt')[0]
 
 # Part 1: How many characters need to be processed before the first
 # start-of-packet marker is detected?
-START_MARKER_LEN = 4
+print(f'Part 1 answer: {find_x_unique_chars(input_stream, 4)}')
 
-char_count = {}
-for i in range(0,len(stream)):
-  # remove 5th oldest character from count
-  if i >= START_MARKER_LEN:
-    dropped_char = stream[i-START_MARKER_LEN]
-    char_count[dropped_char] -= 1
-    if char_count[dropped_char] == 0:
-      char_count.pop(dropped_char)
-  # add in newest character
-  # (dictionary value default is 0)
-  if stream[i] not in char_count:
-    char_count[stream[i]] = 0
-  char_count[stream[i]] += 1
-  # check how many unique characters have been seen
-  if len(char_count) == START_MARKER_LEN:
-    print(f'Part 1 answer: {i+1}')
-    break
+# Part 2: How many characters need to be processed before the first
+# start-of-message marker is detected?
+print(f'Part 2 answer: {find_x_unique_chars(input_stream, 14)}')
