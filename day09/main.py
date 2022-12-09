4f15076eaffa7ccd56416de2ae5f39d24ce212dd
Laura Galbraith <laura_b_galbraith@msn.com>
2022-12-09 15:08:22 -0800

    Day 9 Puzzle 2 solved

--- day09/main.py
+++ day09/main.py
@@ -36,50 +36,59 @@ class Position:
   def __str__(self):
     return f'({self.row},{self.col})'
 
-  def move(self, direction):
+  def move_in_direction(self, direction):
     d = DIRECTIONS.index(direction)
     self.row += row_dirs[d]
     self.col += col_dirs[d]
 
-  def touching(self, other_position):
-    return (-1 <= self.row - other_position.row <= 1) and \
-           (-1 <= self.col - other_position.col <= 1)
+  def move_to_touch(self, other):
+    if not self.touching(other):
+      # diagonal move combines:
+      # vertical move
+      if other.row != self.row:
+        self.row += (1 if other.row > self.row else -1)
+      # horizontal move
+      if other.col != self.col:
+        self.col += (1 if other.col > self.col else -1)
+
+  def touching(self, other):
+    return (-1 <= self.row - other.row <= 1) and \
+           (-1 <= self.col - other.col <= 1)
+
+def count_tail_positions_over_motions(motions, knots):
+  """Return the number of positions the tail has been at"""
+  tail_positions = set()
+  tail_positions.add(knots[-1])
+
+  # Simulate motions to count tail positions
+  for motion in motions:
+    for _ in range(int(motion[2:])):
+      # move head
+      knots[0].move_in_direction(motion[0])
+
+      # move each following knot
+      for k in range(1,len(knots)):
+        knots[k].move_to_touch(knots[k-1])
+
+      # count tail position
+      tail_positions.add(knots[-1])
+
+  return len(tail_positions)
 
 GRID_SIDE_MAX = 2000 * 19 + 2  # number of input lines and maximum step size
 GRID_MID_POINT = int(GRID_SIDE_MAX/2)
-# using list rather than tuple so it's mutable
-head_pos = Position(GRID_MID_POINT, GRID_MID_POINT)
-tail_pos = Position(GRID_MID_POINT, GRID_MID_POINT)
-tail_positions = set()
-tail_positions.add(tail_pos)
 
 # Parse input
-motions = read_ascii_file_lines('input.txt')
+motions_input = read_ascii_file_lines('input.txt')
 
 # Part 1
-# Simulate motions to count tail positions
-for motion in motions:
-  # print(f'Moving {motion[0]} by {int(motion[2:])}')
-  for _ in range(int(motion[2:])):
-    # move head
-    head_pos.move(motion[0])
+answer = count_tail_positions_over_motions(motions_input,
+  [Position(GRID_MID_POINT, GRID_MID_POINT) for _ in range(2)])
+print(f'Part 1 answer: {answer}')
 
-    # check if tail is no longer "touching" head, and move it if so
-    if not tail_pos.touching(head_pos):
-      # print(f'non-touching Head: {head_pos}, Tail: {tail_pos}')
-
-      # diagonal case: move one step diagonally
-      if head_pos.row != tail_pos.row and head_pos.col != tail_pos.col:
-        tail_pos.row += (1 if head_pos.row > tail_pos.row else -1)
-        tail_pos.col += (1 if head_pos.col > tail_pos.col else -1)
-
-      # lateral case: move in same direction as head
-      else:
-        tail_pos.move(motion[0])
-
-    # count tail position
-    # print(f'Final Head: {head_pos}, Tail: {tail_pos}')
-    tail_positions.add(tail_pos)
-    # print(f'Added {tail_pos} to set and size is now {len(tail_positions)}')
-
-print(f'Part 1 answer: {len(tail_positions)}')
+# Part 2
+# Simulate your complete series of motions on a larger rope with ten knots.
+# How many positions does the tail of the rope visit at least once?
+answer = count_tail_positions_over_motions(motions_input,
+  [Position(GRID_MID_POINT, GRID_MID_POINT) for _ in range(10)])
+print(f'Part 2 answer: {answer}')
