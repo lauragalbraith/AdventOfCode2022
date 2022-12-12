04ae789c3084c73e834ebb2b8431afe9884b7cbc
Laura Galbraith <laura_b_galbraith@msn.com>
2022-12-12 11:29:05 -0800

    Day 11 Part 2: working up to round 1000 with example input, but very slow

--- day11/main.go
+++ day11/main.go
@@ -11,6 +11,7 @@ import (
 	"bufio"
 	"container/list" // https://pkg.go.dev/container/list
 	"fmt"
+	"math/big" // https://pkg.go.dev/math/big
 	"os"
 	"regexp"
 	"strconv"
@@ -39,20 +40,17 @@ func GetLinesFromFile(name string) ([]string, error) {
 	return lines, nil
 }
 
-type Operation func(old int64) (new int64)
-
-func Add(old, literal int64) (new int64)    { return old + literal }
-func Multiply(old, other int64) (new int64) { return old * other }
+type Operation func(old *big.Int) (new *big.Int)
 
 type Monkey struct {
 	id             int
 	items          *list.List
 	inspect_op     Operation
-	divisible_test int64
+	divisible_test *big.Int
 	true_dest      int
 	false_dest     int
 
-	inspections_performed uint
+	inspections_performed uint64
 }
 
 func (m *Monkey) String() string {
@@ -68,9 +66,37 @@ func (m *Monkey) String() string {
 	return sb.String()
 }
 
+func (m *Monkey) DeepCopy() *Monkey {
+	if m == nil {
+		return nil
+	}
+
+	new_m := new(Monkey)
+
+	new_m.id = m.id
+	new_m.items = list.New()
+	for e := m.items.Front(); e != nil; e = e.Next() {
+		var original_e *big.Int
+		original_e = e.Value.(*big.Int)
+		if !original_e.IsInt64() {
+			panic(fmt.Errorf("Item value %v inside monkey %d cannot be copied\n", original_e, m.id))
+		}
+
+		new_m.items.PushBack(big.NewInt(original_e.Int64()))
+	}
+	new_m.inspect_op = m.inspect_op
+	new_m.divisible_test = m.divisible_test
+	new_m.true_dest = m.true_dest
+	new_m.false_dest = m.false_dest
+	new_m.inspections_performed = m.inspections_performed
+
+	return new_m
+}
+
 func ParseMonkeysFromInput(lines []string) ([]*Monkey, error) {
 	// Initialize monkeys
-	monkeys := make([]*Monkey, 8) // cheat a little, see input
+	monkeys := make([]*Monkey, (len(lines)+1)/7)
+	// fmt.Printf("There are %v lines so there are %v monkeys\n", len(lines), len(monkeys))
 
 	operation_line_re := regexp.MustCompile(`^  Operation: new = old (.) (.+)$`)
 	test_line_re := regexp.MustCompile(`^  Test: divisible by (\d+)$`)
@@ -102,7 +128,7 @@ func ParseMonkeysFromInput(lines []string) ([]*Monkey, error) {
 				return nil, err
 			}
 
-			m.items.PushBack(int64(num))
+			m.items.PushBack(big.NewInt(num))
 		}
 
 		i++
@@ -117,7 +143,7 @@ func ParseMonkeysFromInput(lines []string) ([]*Monkey, error) {
 
 		if strings.Compare(op_details[0][1], "*") == 0 {
 			if strings.Compare(op_details[0][2], "old") == 0 {
-				m.inspect_op = func(old int64) (new int64) { return Multiply(old, old) }
+				m.inspect_op = func(old *big.Int) (new *big.Int) { return old.Mul(old, old) }
 			} else {
 				literal, err := strconv.ParseInt(op_details[0][2], 10, 64)
 				if err != nil {
@@ -125,7 +151,7 @@ func ParseMonkeysFromInput(lines []string) ([]*Monkey, error) {
 					return nil, err
 				}
 
-				m.inspect_op = func(old int64) (new int64) { return Multiply(old, literal) }
+				m.inspect_op = func(old *big.Int) (new *big.Int) { return old.Mul(old, big.NewInt(literal)) }
 			}
 		} else {
 			// addition
@@ -135,7 +161,7 @@ func ParseMonkeysFromInput(lines []string) ([]*Monkey, error) {
 				return nil, err
 			}
 
-			m.inspect_op = func(old int64) (new int64) { return Add(old, literal) }
+			m.inspect_op = func(old *big.Int) (new *big.Int) { return old.Add(old, big.NewInt(literal)) }
 		}
 
 		i++
@@ -147,11 +173,13 @@ func ParseMonkeysFromInput(lines []string) ([]*Monkey, error) {
 			return nil, err
 		}
 
-		m.divisible_test, err = strconv.ParseInt(test_details[0][1], 10, 64)
+		div_64, err := strconv.ParseInt(test_details[0][1], 10, 64)
 		if err != nil {
 			fmt.Printf("Unable to extract division literal from line '%s': %+v\n", lines[i], err)
 			return nil, err
 		}
+
+		m.divisible_test = big.NewInt(div_64)
 
 		i++
 
@@ -193,38 +221,41 @@ func ParseMonkeysFromInput(lines []string) ([]*Monkey, error) {
 	return monkeys, nil
 }
 
-func main() {
-	// Get input from file
-	lines, err := GetLinesFromFile("input.txt")
-	if err != nil {
-		panic(err)
-	}
+func MonkeyBusiness(monkeys []*Monkey, rounds uint, undamaged_bonus bool) uint64 {
+	// for _, m := range monkeys {
+	// fmt.Printf("DEBUG: monkey %+v has inspected items %d times\n", m, m.inspections_performed)
+	// }
 
-	monkeys, err := ParseMonkeysFromInput(lines)
-	if err != nil {
-		panic(err)
-	}
-
-	// Emulate monkeys throwing for 20 rounds
-	for round := 1; round <= 20; round++ {
+	// Emulate monkeys throwing for X rounds
+	var round uint
+	for round = 1; round <= rounds; round++ {
+		if round%100 == 0 {
+			fmt.Printf("Round %d\n", round)
+		}
 		for _, m := range monkeys {
 			num_items := m.items.Len()
 			for i := 0; i < num_items; i++ {
 				// monkey inspects item
 				item := m.items.Front()
-				worry_level := item.Value.(int64)
+				worry_level := item.Value.(*big.Int)
 				m.items.Remove(item)
 				m.inspections_performed++
 
 				// worry level goes through operation
 				worry_level = m.inspect_op(worry_level)
 
-				// monkey gets bored with item; worry level is divided by 3
-				worry_level /= 3
+				// monkey gets bored with item; worry level may be divided by 3
+				if undamaged_bonus {
+					worry_level, _ = worry_level.DivMod(worry_level, big.NewInt(3), big.NewInt(0))
+				}
 
 				// test worry level against monkey's condition
 				dest_monkey := m.false_dest
-				if worry_level%(m.divisible_test) == 0 {
+
+				remainder := big.NewInt(0)
+				quotient := big.NewInt(0)
+				quotient, remainder = quotient.DivMod(worry_level, m.divisible_test, remainder)
+				if remainder.IsInt64() && remainder.Int64() == 0 {
 					dest_monkey = m.true_dest
 				}
 
@@ -233,10 +264,17 @@ func main() {
 				monkeys[dest_monkey].items.PushBack(worry_level)
 			}
 		}
+
+		if round == 1 || round == 20 || round%1000 == 0 {
+			fmt.Printf("DEBUG: after round %d...\n", round)
+			for _, m := range monkeys {
+				fmt.Printf("DEBUG: monkey %+v inspected items %d times\n", m.id, m.inspections_performed)
+			}
+		}
 	}
 
 	// Determine two most active monkeys
-	var most, second_most uint
+	var most, second_most uint64
 	most = 0
 	second_most = 0
 
@@ -251,6 +289,36 @@ func main() {
 	}
 
 	// Calculate the level of monkey business
-	monkey_business_level := most * second_most
+	return most * second_most
+}
+
+func main() {
+	// Get input from file
+	lines, err := GetLinesFromFile("example_input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	initial_monkeys, err := ParseMonkeysFromInput(lines)
+	if err != nil {
+		panic(err)
+	}
+
+	// Part 1
+	// worry is divided by 3 each inspection, 20 rounds
+	monkeys := make([]*Monkey, len(initial_monkeys))
+	for i, m := range initial_monkeys {
+		// fmt.Printf("DEBUG: Copying monkey %+v\n", m)
+		monkeys[i] = m.DeepCopy()
+	}
+
+	monkey_business_level := MonkeyBusiness(monkeys, 20, true)
 	fmt.Printf("Part 1 answer: %v\n", monkey_business_level)
+
+	// Part 2
+	// Starting again from the initial state in your puzzle input, what is the level of monkey business after 10000 rounds?
+	monkey_business_level = MonkeyBusiness(initial_monkeys, 10000, false)
+	fmt.Printf("Part 2 answer: %v\n", monkey_business_level) // TODO 26823519255 is too high; round 20 numbers are correct, but rund 1000 is where it starts to differ (all monkeys differ)
+	// TODO NEXT debug ideas: see if any true/false branches are not taken prior to round 21; see if there's list limits that are being hit; see if there are any variables that are changing during emulation that shouldn't be
+	// TODO HERE our int type is too small to hold some of the numbers; numbers are becoming negative
 }
