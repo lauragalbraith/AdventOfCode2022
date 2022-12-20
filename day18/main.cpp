812062e5ffebe0c0fb939d20721488e93416c24d
Laura Galbraith <laura_b_galbraith@msn.com>
2022-12-20 11:04:36 -0800

    Day 18: stub out files

---
+++ day18/main.cpp
@@ -0,0 +1,16 @@
+// main.cpp: Laura Galbraith
+// Description: Solve Day 18 of Advent of Code 2022
+// TODO title
+// Compile and run: rm main.out ; g++ -std=c++17 -static-liblsan -fsanitize=leak main.cpp -o main.out -Wall -Werror -Wextra -pedantic -Wshadow -Wconversion -fmax-errors=2 && ./main.out
+
+#include <iostream>  // cout, endl
+
+using namespace std;
+
+int main() {
+  // Part 1
+  // TODO implement local library for parsing input file
+
+  cout << endl << "Part 1 answer: " << "TODO" << endl;
+  return 0;
+}
